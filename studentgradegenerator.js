5ee14e8c4cd3ca09623b47615843410d2eea368d
Sammybett-bit <bettsammy20@gmail.com>
2023-07-14 19:30:06 +0300

    js created

---
+++ studentgradegenerator.js
@@ -0,0 +1,19 @@
+let studentMark = 90
+if (studentMarks >= 80 && studentMarks <= 100) {
+    console.log('You got A');
+  }
+  else if(studentMarks >= 70 && studentMarks <= 80){
+    console.log('You got B');
+  }
+  else if(studentMarks >= 60 && studentMarks <= 70){
+    console.log('You got C');
+  }
+  else if(studentMarks >= 50 && studentMarks <= 60){
+    console.log('You got D')
+  }
+  else if(studentMarks >= 40 && studentMarks <= 50){
+    console.log('You got E')
+  }
+  else {
+    console.log('OUT OF RANGE')
+  }
