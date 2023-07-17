9b74a8ef63ca06ac638c8a62a11d4c5763cd561a
Sammybett-bit <bettsammy20@gmail.com>
2023-07-17 08:41:01 +0300

    Completed assignment

--- NetSalaryCalculator.js
+++ NetSalaryCalculator.js
@@ -36,8 +36,35 @@ function nhifDeductions() {
     nhif = 150;
   } else if (grossSalary >= 5999 && grossSalary <= 7999) {
     nhif = 300;
-  } // ... other salary ranges ...
-  else if (grossSalary >= 99999) {
+  } else if (grossSalary >= 7999 && grossSalary <= 11999) {
+    nhif = 400;
+  } else if (grossSalary >= 11999 && grossSalary <= 14999) {
+    nhif = 500;
+  } else if (grossSalary >= 14999 && grossSalary <= 19999) {
+    nhif = 600;
+  } else if (grossSalary >= 19999 && grossSalary <= 24999) {
+    nhif = 750;
+  } else if (grossSalary >= 24999 && grossSalary <= 29999) {
+    nhif = 850;
+  } else if (grossSalary >= 29999 && grossSalary <= 34999) {
+    nhif = 900;
+  } else if (grossSalary >= 34999 && grossSalary <= 39999) {
+    nhif = 950;
+  } else if (grossSalary >= 39999 && grossSalary <= 44999) {
+    nhif = 1000;
+  } else if (grossSalary >= 44999 && grossSalary <= 49999) {
+    nhif = 1100;
+  } else if (grossSalary >= 49999 && grossSalary <= 59999) {
+    nhif = 1200;
+  } else if (grossSalary >= 59999 && grossSalary <= 69999) {
+    nhif = 1300;
+  } else if (grossSalary >= 69999 && grossSalary <= 79999) {
+    nhif = 1400;
+  } else if (grossSalary >= 79999 && grossSalary <= 89999) {
+    nhif = 1500;
+  } else if (grossSalary >= 89999 && grossSalary <= 99999) {
+    nhif = 1600;
+  } else if (grossSalary >= 99999) {
     nhif = 1700;
   }
   // Output the nhif value to the console
